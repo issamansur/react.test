2f06f2f73499266f7b4f64476c80f1d2d064e4ac
User#None <77580844+issamansur@users.noreply.github.com>
2025-03-16 22:00:59 +0000

    create folders for components for every lab

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Gretting from './Greeting';
-import CurrentDateTime from './CurrentDateTime';
+import Gretting from './lab1/Greeting';
+import CurrentDateTime from './lab1/CurrentDateTime';
 
 function App() {
   return (
